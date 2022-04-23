ef1424e0e6638ede79fb66424bf342407e7f9f48
Alyssa Sitto <alyssa.sitto2@gmail.com>
2022-04-22 21:22:31 -0700

    Edited css, index, and js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const form = document.querySelector(".cta-form");
+const email = document.querySelector("#email");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	validateForm();
+});
+
+function validateForm() {
+	const emailVal = email.value.trim();
+
+	if (emailVal === "") {
+		form.classList.add("error");
+	} else if (emailVal !== "") {
+		form.classList.remove("error");
+		form.reset();
+	}
+}
